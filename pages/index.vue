491abf940d833b535a7b5ad6acf83d33c186340f
GaoNeng <31283122+GaoNeng-wWw@users.noreply.github.com>
2025-03-04 20:38:22 +0800

    Revert "feat: color switch"

--- pages/index.vue
+++ pages/index.vue
@@ -1,14 +1,11 @@
 <template>
   <div class="w-full">
-    <lazy-app-header class="z-10" />
+    <app-header class="z-10" />
     <div class="mx-auto size-full max-w-screen-md px-2 pt-8">
       <nuxt-page />
       <footer>
         <lazy-app-footer />
       </footer>
-    </div>
-    <div class="fixed right-8 top-3/4 size-fit">
-      <app-color-switch />
     </div>
   </div>
 </template>
